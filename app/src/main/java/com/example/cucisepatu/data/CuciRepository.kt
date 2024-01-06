44bb336312889e387592f47577bb99f0086f863d
Ghozi <mghozighifar04@gmail.com>
2024-01-06 15:31:24 +0700

    Membuat function untuk Repository Firestore

--- app/src/main/java/com/example/cucisepatu/data/CuciRepository.kt
+++ app/src/main/java/com/example/cucisepatu/data/CuciRepository.kt
@@ -1,6 +1,7 @@
 package com.example.cucisepatu.data
 
 import com.example.cucisepatu.model.Sepatu
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.flow.Flow
 
 interface CuciRepository{
@@ -10,3 +11,28 @@ interface CuciRepository{
     suspend fun delete (sepatuId: String)
     fun getSepatuById(sepatuId: String) : Flow<Sepatu>
 }
+
+class CuciSepatuRepository(private val firestore: FirebaseFirestore) : CuciRepository{
+    override fun getAll(): Flow<List<Sepatu>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun save(sepatu: Sepatu): String {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun update(sepatu: Sepatu) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun delete(sepatuId: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getSepatuById(sepatuId: String): Flow<Sepatu> {
+        TODO("Not yet implemented")
+    }
+
+}
+
+
