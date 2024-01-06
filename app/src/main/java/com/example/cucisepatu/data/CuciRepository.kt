13b7e5684cef9316c8af0ed1df3e9017f2c34ce9
Ghozi <mghozighifar04@gmail.com>
2024-01-06 15:33:45 +0700

    Mengambil semua data sepatu dari Firestore dengan urutan nama.

--- app/src/main/java/com/example/cucisepatu/data/CuciRepository.kt
+++ app/src/main/java/com/example/cucisepatu/data/CuciRepository.kt
@@ -2,7 +2,12 @@ package com.example.cucisepatu.data
 
 import com.example.cucisepatu.model.Sepatu
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.tasks.await
 
 interface CuciRepository{
     fun getAll() : Flow<List<Sepatu>>
@@ -13,9 +18,14 @@ interface CuciRepository{
 }
 
 class CuciSepatuRepository(private val firestore: FirebaseFirestore) : CuciRepository{
-    override fun getAll(): Flow<List<Sepatu>> {
-        TODO("Not yet implemented")
-    }
+    override fun getAll(): Flow<List<Sepatu>> = flow {
+        val snapshot = firestore.collection("Sepatu")
+            .orderBy("Nama", Query.Direction.ASCENDING)
+            .get()
+            .await()
+        val kontak = snapshot.toObjects(Sepatu::class.java)
+        emit(kontak)
+    } .flowOn(Dispatchers.IO)
 
     override suspend fun save(sepatu: Sepatu): String {
         TODO("Not yet implemented")
