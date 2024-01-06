ae7fc9bbef3f04904f6f471a929d0357c033267a
Ghozi <mghozighifar04@gmail.com>
2024-01-06 15:50:08 +0700

    Membuat antarmuka dan implementasi Firebase untuk repositori pencucian sepatu.

---
+++ app/src/main/java/com/example/cucisepatu/data/CuciFireBase.kt
@@ -0,0 +1,15 @@
+package com.example.cucisepatu.data
+
+import com.google.firebase.firestore.FirebaseFirestore
+
+interface CuciSepatuFireBase {
+    val cuciRepository : CuciRepository
+}
+
+class CuciFireBase : CuciSepatuFireBase {
+    private val firestore :FirebaseFirestore = FirebaseFirestore.getInstance()
+
+    override val cuciRepository: CuciRepository by lazy {
+        CuciSepatuRepository(firestore)
+    }
+}
