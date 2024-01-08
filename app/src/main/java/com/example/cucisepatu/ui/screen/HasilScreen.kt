f8c74e1066d69900f29709515c3dfedac88da7af
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2024-01-08 10:49:26 +0700

    Membuat function OnError dan OnLoading

--- app/src/main/java/com/example/cucisepatu/ui/screen/HasilScreen.kt
+++ app/src/main/java/com/example/cucisepatu/ui/screen/HasilScreen.kt
@@ -1,5 +1,6 @@
 package com.example.cucisepatu.ui.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,10 +9,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -21,7 +25,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.cucisepatu.R
 import com.example.cucisepatu.model.Sepatu
 import com.example.cucisepatu.navigation.DestinasiNavigasi
 
@@ -31,13 +38,55 @@ object DestinasiHasil : DestinasiNavigasi {
 }
 
 @Composable
+fun OnLoading(modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.loading_img),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+
+@Composable
+fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_connection_error), contentDescription = ""
+        )
+        Text(text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
+
+@Composable
 fun PesanLayout(
     sepatu: List<Sepatu>,
     modifier: Modifier = Modifier,
     onDetailClick: (Sepatu) -> Unit,
     onDeleteClick: (Sepatu) -> Unit = {}
 ) {
-
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ){
+        items(sepatu) {sepatu ->
+            PesanCard(
+                sepatu = sepatu,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onDetailClick(sepatu) },
+                onDeleteClick = {
+                    onDeleteClick(sepatu)
+                }
+            )
+        }
+    }
 }
 
 @Composable
