6bccf290f242a1aad06f63e8ff63bd4c56cdb250
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2024-01-08 10:15:16 +0700

    Menambahkan text untuk nohp, tipe sepatu, dan tipe cuci

--- app/src/main/java/com/example/cucisepatu/model/Sepatu.kt
+++ app/src/main/java/com/example/cucisepatu/model/Sepatu.kt
@@ -3,8 +3,9 @@ package com.example.cucisepatu.model
 data class Sepatu (
     val id : String,
     val nama : String,
+    val nohp : String,
     val tipe_sepatu : String,
     val tipe_cuci : String
 ){
-    constructor() : this ("","","","")
+    constructor() : this ("","","","","")
 }
--- app/src/main/java/com/example/cucisepatu/ui/screen/HasilScreen.kt
+++ app/src/main/java/com/example/cucisepatu/ui/screen/HasilScreen.kt
@@ -3,13 +3,20 @@ package com.example.cucisepatu.ui.screen
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.cucisepatu.model.Sepatu
@@ -42,6 +49,34 @@ fun PesanCard(
                     text = sepatu.nama,
                     style = MaterialTheme.typography.titleLarge,
                 )
+                Spacer(Modifier.weight(1f))
+                Icon(
+                    imageVector = Icons.Default.Phone,
+                    contentDescription = null,
+                )
+                Text(
+                    text = sepatu.nohp,
+                    style = MaterialTheme.typography.titleMedium
+                )
+            }
+            Row(modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = sepatu.tipe_sepatu,
+                    style = MaterialTheme.typography.titleMedium
+                )
+                Text(
+                    text = sepatu.tipe_cuci,
+                    style = MaterialTheme.typography.titleMedium
+                )
+                Spacer(Modifier.weight(1f))
+                IconButton(onClick = { onDeleteClick(sepatu) }) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = null,
+                    )
+                }
             }
         }
     }
