e5318e54eada9eb1a76a9c4d8a263a042501561a
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2024-01-08 09:30:43 +0700

    Buat object DestinasiHasil di HasilScreen

--- app/src/main/java/com/example/cucisepatu/ui/screen/HasilScreen.kt
+++ app/src/main/java/com/example/cucisepatu/ui/screen/HasilScreen.kt
@@ -1,2 +1,8 @@
 package com.example.cucisepatu.ui.screen
 
+import com.example.cucisepatu.navigation.DestinasiNavigasi
+
+object DestinasiHasil : DestinasiNavigasi {
+    override val route = "hasil"
+    override val titleRes = "Sepatu"
+}
