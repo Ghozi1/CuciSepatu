518ac9a2ea58a700e2bd108bbc108f6710e1d186
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2024-01-08 09:34:55 +0700

    Menentukan parameter untuk function PesanCard

--- app/src/main/java/com/example/cucisepatu/ui/screen/HasilScreen.kt
+++ app/src/main/java/com/example/cucisepatu/ui/screen/HasilScreen.kt
@@ -1,8 +1,18 @@
 package com.example.cucisepatu.ui.screen
 
+import androidx.compose.ui.Modifier
+import com.example.cucisepatu.model.Sepatu
 import com.example.cucisepatu.navigation.DestinasiNavigasi
 
 object DestinasiHasil : DestinasiNavigasi {
     override val route = "hasil"
     override val titleRes = "Sepatu"
+}
+
+fun PesanCard(
+    sepatu: Sepatu,
+    onDeleteClick: (Sepatu) -> Unit = {},
+    modifier: Modifier = Modifier
+){
+
 }
