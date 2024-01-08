d20603e5c07631b117bc2189697705126a79d4ce
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2024-01-08 10:20:46 +0700

    Membuat function PesanLayout

--- app/src/main/java/com/example/cucisepatu/ui/screen/HasilScreen.kt
+++ app/src/main/java/com/example/cucisepatu/ui/screen/HasilScreen.kt
@@ -1,11 +1,14 @@
 package com.example.cucisepatu.ui.screen
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Phone
@@ -25,6 +28,16 @@ import com.example.cucisepatu.navigation.DestinasiNavigasi
 object DestinasiHasil : DestinasiNavigasi {
     override val route = "hasil"
     override val titleRes = "Sepatu"
+}
+
+@Composable
+fun PesanLayout(
+    sepatu: List<Sepatu>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Sepatu) -> Unit,
+    onDeleteClick: (Sepatu) -> Unit = {}
+) {
+
 }
 
 @Composable
