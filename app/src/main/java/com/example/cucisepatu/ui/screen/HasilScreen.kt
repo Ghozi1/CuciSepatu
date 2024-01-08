8c4017e29c51e7258d7122fcc5d705226e279836
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2024-01-08 10:02:25 +0700

    Membuat card dan column di function PesanCard

--- app/src/main/java/com/example/cucisepatu/ui/screen/HasilScreen.kt
+++ app/src/main/java/com/example/cucisepatu/ui/screen/HasilScreen.kt
@@ -1,6 +1,14 @@
 package com.example.cucisepatu.ui.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.cucisepatu.model.Sepatu
 import com.example.cucisepatu.navigation.DestinasiNavigasi
 
@@ -9,10 +17,22 @@ object DestinasiHasil : DestinasiNavigasi {
     override val titleRes = "Sepatu"
 }
 
+@Composable
 fun PesanCard(
     sepatu: Sepatu,
     onDeleteClick: (Sepatu) -> Unit = {},
     modifier: Modifier = Modifier
 ){
+    Card (
+        modifier = modifier,
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ){
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
 
+        }
+    }
 }
