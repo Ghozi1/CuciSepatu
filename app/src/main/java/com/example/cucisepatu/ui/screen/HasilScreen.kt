22e7ffa11d62ba0c3ac02661745605628d3a1132
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2024-01-08 10:05:47 +0700

    Menambahkan text untuk nama pemesan di column

--- app/src/main/java/com/example/cucisepatu/ui/screen/HasilScreen.kt
+++ app/src/main/java/com/example/cucisepatu/ui/screen/HasilScreen.kt
@@ -2,10 +2,13 @@ package com.example.cucisepatu.ui.screen
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -32,7 +35,14 @@ fun PesanCard(
             modifier = Modifier.padding(16.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
-
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                Text(
+                    text = sepatu.nama,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+            }
         }
     }
 }
